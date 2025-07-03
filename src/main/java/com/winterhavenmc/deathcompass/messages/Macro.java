54c01537dff8d21e76c2459700ecbd1ed008f333
Tim Savage <tim.savage@gmail.com>
2025-07-02 20:42:59 -0500

    edit comment

--- src/main/java/com/winterhavenmc/deathcompass/messages/Macro.java
+++ src/main/java/com/winterhavenmc/deathcompass/messages/Macro.java
@@ -19,6 +19,6 @@ package com.winterhavenmc.deathcompass.messages;
 
 public enum Macro {
 
-	// No macros defined; this plugin only uses built-in macros %ITEM%, %USER%, %WORLD%
+	// No macros defined; this plugin only uses built-in macros %ITEM%, %PLAYER%, %WORLD%
 
 }
